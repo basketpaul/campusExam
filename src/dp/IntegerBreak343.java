a98d19f00deaf959459ca74aba91a5918ad833b4
paul <122125597@qq.com>
2019-07-23 21:31:48 +0800

    343.整数拆分 279.完全平方数

---
+++ src/dp/IntegerBreak343.java
@@ -0,0 +1,18 @@
+package dp;
+
+public class IntegerBreak343 {
+    public static void main(String[] args) {
+        System.out.println(integerBreak(5));
+    }
+
+    public static int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i], Math.max(j * dp[i - j], j * (i - j)));
+            }
+        }
+        return dp[n];
+    }
+}
---
+++ src/dp/PerfectSquares279.java
@@ -0,0 +1,19 @@
+package dp;
+
+public class PerfectSquares279 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 1; i - j * j >= 0; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+}
