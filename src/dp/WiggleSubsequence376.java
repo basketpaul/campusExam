8a4a28eeb6a38fd25ef5125e8dc56796c1c58b14
paul <122125597@qq.com>
2019-07-24 16:08:00 +0800

    376.摆动序列

---
+++ src/dp/WiggleSubsequence376.java
@@ -0,0 +1,20 @@
+package dp;
+
+public class WiggleSubsequence376 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int wiggleMaxLength(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        int up = 1, down = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1])
+                up = down + 1;
+            else if (nums[i] < nums[i - 1])
+                down = up + 1;
+        }
+        return Math.max(up, down);
+    }
+}
