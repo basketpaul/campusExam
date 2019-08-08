f1c7af95d9f55ca135cd372b7d28ebffec549030
paul <122125597@qq.com>
2019-08-08 15:54:46 +0800

    包含min的Stack

---
+++ src/offer/MinStack.java
@@ -0,0 +1,29 @@
+package offer;
+
+import java.util.Stack;
+
+public class MinStack {
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minStack = new Stack<>();
+
+    public void push(int node) {
+        stack.push(node);
+        if (minStack.isEmpty())
+            minStack.push(node);
+        else
+            minStack.push(Math.min(minStack.peek(), node));
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int min() {
+        return minStack.peek();
+    }
+}
